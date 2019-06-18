3c9923d3f59d4134cd3995b8a4ea1f91e17f94e4
Deviganesan <51699366+Deviganesan@users.noreply.github.com>
2019-06-18 17:47:26 +0530

    Create primenumber.c

---
+++ primenumber.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a,i;
+    scanf("%d",&a);
+    for(i=2;i<a/2;i++)
+    {
+
+    }
+        if(a%i==0){
+            printf("no");
+            }
+
+        else
+        {
+
+            printf("yes");
+    }
+}
